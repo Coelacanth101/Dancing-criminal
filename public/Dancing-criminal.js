fda46122df29b319da291e0f549d928e7db33c35
Coelacanth101 <106444731+Coelacanth101@users.noreply.github.com>
2022-06-21 00:09:57 +0900

    Update Dancing-criminal.js

--- public/Dancing-criminal.js
+++ public/Dancing-criminal.js
@@ -290,7 +290,7 @@ socket.on('playboy', (data)=>{
     }else{
         $('#whoseturn').html(`<p><strong>${data.turnPlayer.name}が犯人カードを確認しています。</strong></p>`);
     } 
-    if(socket.id === dogchoicedata.turnPlayer.socketid){
+    if(socket.id === data.turnPlayer.socketID){
         $('#checkbuttonarea').show();
     };
 })
